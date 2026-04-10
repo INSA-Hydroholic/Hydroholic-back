16e6ace90b93e9f06dc8b333a45ce40e903a8899
Jhouny <jhonyaraujo9121@gmail.com>
2026-04-10 12:06:06 +0200

    Added adapter for PrismaClient configuration in v7

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,2 +1,8 @@
 import { PrismaClient } from '@prisma/client';
-export const prisma = new PrismaClient();
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const adapter = new PrismaPg({
+    connectionString: `postgresql://${process.env["POSTGRES_USERNAME"]}:${process.env["POSTGRES_PASSWORD"]}@${process.env["POSTGRES_HOST"]}:${process.env["POSTGRES_PORT"]}/${process.env["POSTGRES_DB"]}?schema=public`,
+});
+
+export const prisma = new PrismaClient({ adapter });
